1dc57a47c2e58e20be236dd0b56ecbd31674d4ea
Nameuncoxali <Emailalif.mohamady20@gmail.com>
2020-11-05 18:21:39 +0330

    demo file saman

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path')
 
 const app = express();
 
-app.use('/', serverStatic(path.join(__dirname, '/dist')))
+app.use('/', serverStatic(path.join(__dirname, '/dist/index.html')))
 
 const port = process.env.PORT || 8080
 
